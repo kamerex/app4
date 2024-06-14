4fbc4268d6e94778bc5bf0ff7cf943da3eeb0a49
kamerex <cedrprk@gmail.com>
2024-06-13 21:22:46 -0400

    Initial commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,56 @@
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { View,  Text, StyleSheet } from 'react-native';
+import { ThemedText } from '@/components/ThemedText';
+import Deck from '@/components/Deck';
+
+const DATA = [
+  { id: 1, text: 'Card #1', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
+  { id: 2, text: 'Card #2', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
+  { id: 3, text: 'Card #3', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+  { id: 4, text: 'Card #4', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
+  { id: 5, text: 'Card #5', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
+  { id: 6, text: 'Card #6', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
+  { id: 7, text: 'Card #7', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+  { id: 8, text: 'Card #8', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
+];
+
+function renderCard(item){
+  return (
+    <Text>{item.text}</Text>
+  )
+}
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView  style={styles.container} >
+      <Deck
+         data={DATA}
+         renderCard={renderCard}
+        >
+      </Deck>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  container:{
+    flex: 1,
+    backgroundColor: '$fff',
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
